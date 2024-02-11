e400724fc12e4f3d481b99b5f3600a5ce1bb3996
CuttleFish <b9804320975@gmail.com>
2024-02-11 12:53:29 +0800

    add product index proimage ok

--- src/main/java/com/photoalbum/dodo/controller/ProductController.java
+++ src/main/java/com/photoalbum/dodo/controller/ProductController.java
@@ -8,27 +8,43 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Controller
 @RequestMapping("/Products")
 public class ProductController {
 
-    private final ProductServiceImpl productService;
+//    private final ProductServiceImpl productService;
 
     @Autowired
-    public ProductController(ProductService productService) {
-        this.productService = (ProductServiceImpl) productService;
-    }
+    private ProductServiceImpl productServiceImpl;
 
-    @GetMapping("")
+
+    @GetMapping("/")
     public String listProducts(Model model) {
-        model.addAttribute("products", productService.getAllProduct());
+        List<Product> productList = productServiceImpl.getAllProduct();
+        Map<Integer, String> imageMap = new HashMap<>(); // 创建一个新的Map来存储图片数据，假设每个产品都有一个唯一的ID
+
+        for (Product product : productList) {
+            byte[] imageBytes = product.getProimage(); // 假设这是从数据库获取的图片二进制数据
+            if (imageBytes != null) {
+                String imageBase64 = Base64.getEncoder().encodeToString(imageBytes);
+                imageMap.put(product.getProid(), imageBase64);
+            }
+        }
+        model.addAttribute("products", productList);
+        model.addAttribute("images", imageMap);
         return "index";
     }
+
 
     @PostMapping("/insertProducts")
     @ResponseBody
     public Integer insertProduct(@RequestBody Product product) {
-        Product savedProduct = productService.saveProduct(product);
+        Product savedProduct = productServiceImpl.saveProduct(product);
         return savedProduct.getProid();
     }
 }
